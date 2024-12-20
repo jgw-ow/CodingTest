a667e5ff49ab2183700677742ac56ddb1b629649
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2024-12-20 09:43:39 +0900

    [level 1] Title: 약수의 합, Time: 0.20 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,1 @@
-def solution(n):
-    answer = 0
-    for i in range(1, n+1):
-        if n%i==0:
-            answer += i
-    return answer
-        
+solution = lambda n: sum(i for i in range(1, n+1) if n%i==0)
