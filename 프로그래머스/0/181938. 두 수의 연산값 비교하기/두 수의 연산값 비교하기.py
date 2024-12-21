fec79faa435f48da1e9f9e872480a972602ac64a
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2024-12-21 17:40:11 +0900

    [level 0] Title: 두 수의 연산값 비교하기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/0/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,4 +1,1 @@
-def solution(a, b):
-    add = int(str(a) + str(b))
-    multy = 2*a*b
-    return add if add > multy else multy if add < multy else add
+solution = lambda a, b: max(int(str(a) + str(b)), 2*a*b)
