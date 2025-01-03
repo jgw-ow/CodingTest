277abb197d896fff44ca0f7f19a7429c13214410
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-03 21:31:42 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -0,0 +1,9 @@
+def solution(hp):
+    answer = 0
+    if hp%5 == 0:
+        answer += hp//5
+    elif hp%5%3 == 0:
+        answer += hp//5 + hp%5//3
+    else:
+        answer += hp//5 + hp%5//3 + hp%5%3//1
+    return answer
