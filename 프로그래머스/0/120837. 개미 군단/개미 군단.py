18f121e2288fc269048b9d7518f8a8df06d73abb
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-05 20:18:16 +0900

    [level 0] Title: 개미 군단, Time: 0.00 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.py
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.py
@@ -1,9 +1,1 @@
-def solution(hp):
-    answer = 0
-    if hp%5 == 0:
-        answer += hp//5
-    elif hp%5%3 == 0:
-        answer += hp//5 + hp%5//3
-    else:
-        answer += hp//5 + hp%5//3 + hp%5%3//1
-    return answer
+solution = lambda hp: hp//5 + hp%5//3 + hp%5%3//1
