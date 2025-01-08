292fb6914620ac83e5dd9a88cfaca04f6da52ae8
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-08 11:22:56 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.02 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -2,5 +2,4 @@ def solution(seoul):
     answer = 0
     for x in range(0, len(seoul)):
         if seoul[x] == 'Kim':
-            answer += x
-    return f"김서방은 {answer}에 있다"
+            return f"김서방은 {x}에 있다"
