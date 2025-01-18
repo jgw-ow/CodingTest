e00be7c8f5c0a7f46eb4bfac8bfba9f1e5379b4f
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-18 13:50:08 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.00 ms, Memory: 9.95 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.py
@@ -1,6 +1,1 @@
-def solution(array):
-    index = 0
-    for i in range(len(array)):
-        if array[i] == sorted(array)[-1]:
-            index += i
-    return [sorted(array)[-1], index]
+solution = lambda array: [max(array), array.index(max(array))]
