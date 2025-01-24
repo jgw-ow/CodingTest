76f0c5611bcc8120b3c6acca9737b1a8e1557e32
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-24 21:37:19 +0900

    [level 2] Title: 주식가격, Time: 147.17 ms, Memory: 19.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,10 +1,12 @@
 def solution(prices):
     result = []
-    for i in range(len(prices)):
+    for i in range(0, len(prices)):
         sec = 0
-        for j in range(i + 1, len(prices)):
+        for j in range(i+1, len(prices)):
             sec += 1
-            if prices[j] < prices[i]:
+            if prices[i] > prices[j]:
                 break
         result.append(sec)
     return result
+    
+        
