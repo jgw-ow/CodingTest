c9dee2b662acab42119d42846b02d05bba8f04df
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-01-26 04:56:40 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,6 +1,11 @@
+# def solution(n, m):
+#     measure = []
+#     for i in range(1, max([n, m])+1):
+#         if n%i==0 and m%i==0:
+#             measure.append(i)
+#     return [max(measure), n*m//max(measure)]
 def solution(n, m):
-    measure = []
-    for i in range(1, max([n, m])+1):
-        if n%i==0 and m%i==0:
-            measure.append(i)
-    return [max(measure), n*m//max(measure)]
+    gcd = lambda a,b: b if a % b == 0 else gcd(b, a%b)
+    lcm = lambda a,b: n*m//gcd(a, b)
+    return [gcd(n,m), lcm(n,m)]
+    
