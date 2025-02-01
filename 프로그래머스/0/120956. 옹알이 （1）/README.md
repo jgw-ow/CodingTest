da2b5e9b55d416bbe90126a65a7fbd7c76f81088
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-02-02 00:23:42 +0900

    [level 0] Title: 옹알이 (1), Time: 0.05 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120956. 옹알이 （1）/README.md
+++ 프로그래머스/0/120956. 옹알이 （1）/README.md
@@ -1,10 +1,10 @@
 # [level 0] 옹알이 (1) - 120956 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120956) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.11 ms
+메모리: 10.1 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 18일 16:59:45
+2025년 02월 02일 00:23:41
 
 ### 문제 설명
 
--- 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -1,18 +1,30 @@
+# def solution(babbling):
+#     arr = ["aya", "ye", "woo", "ma"]
+#     arr_last = ["aya", "ye", "woo", "ma"]
+#     count = 0
+#     for i in arr:
+#         for j in arr:
+#             if i != j:
+#                 arr_last += [i+j]
+#                 for k in arr:
+#                     if j != k and i != k:
+#                         arr_last += [i+j+k]
+#                         for l in arr:
+#                             if k != l and j != l and i != l:
+#                                 arr_last += [i+j+k+l]
+#     for n in babbling:
+#         if n in arr_last:
+#             count += 1
+#     return count
 def solution(babbling):
-    arr = ["aya", "ye", "woo", "ma"]
-    arr_last = ["aya", "ye", "woo", "ma"]
-    count = 0
-    for i in arr:
-        for j in arr:
-            if i != j:
-                arr_last += [i+j]
-                for k in arr:
-                    if j != k and i != k:
-                        arr_last += [i+j+k]
-                        for l in arr:
-                            if k != l and j != l and i != l:
-                                arr_last += [i+j+k+l]
-    for n in babbling:
-        if n in arr_last:
+    valid_words = {"aya", "ye", "woo", "ma"}  # 발음할 수 있는 단어들
+    count = 0  # 발음 가능한 단어 개수
+    
+    for word in babbling:
+        temp = word
+        for v in valid_words:
+            temp = temp.replace(v, " ")  # 발음 가능한 단어들을 공백으로 변환
+        if temp.strip() == "":  # 공백만 남아있다면, 유효한 단어
             count += 1
+
     return count
