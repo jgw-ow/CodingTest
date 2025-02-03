2246edb66ccc7c279e1064cca2a392b6a9835db9
jgw-ow <143059243+jgw-ow@users.noreply.github.com>
2025-02-03 21:31:45 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.py
@@ -1,11 +1,1 @@
-def solution(myString, pat):
-    rev_str = ''
-    for i in range(0, len(myString)):
-        if myString[i] == 'A':
-            rev_str += 'B'
-        else:
-            rev_str += 'A'
-    if pat in rev_str:
-        return 1
-    else:
-        return 0
+solution = lambda myString, pat: int(pat in myString.replace('A', 'C').replace('B', 'A').replace('C', 'B'))
